10461697d54f59109c007261333481d5353fb65d
Levanti F <levanti.francesco@gmail.com>
2023-03-03 22:45:53 +0000

     ...

--- main.go
+++ main.go
@@ -8,44 +8,87 @@ import (
 	"time"
 )
 
+type statsT struct {
+	CurrentOpenConns  int
+	CurrentInUseConns int
+	CurrentIdleConn   int
+	SelectsFired      int
+	SelectsCompleted  int
+	SelectsRunning    int
+	UnixTime          int
+}
+
 var db *sql.DB
 var mysqlMaxOpenConns = 5000
 var maxOpenConns = 50
 var maxIdleConns = 10
 var connMaxLifetime = 3
 var connMaxIdleTime = 1
-var selectsToRun = 1000
+var selectsToRun = 100
 var selectsFired = 0
 var selectsCompleted = 0
-var selectSleepSec = 10
+var selectSleepSec = 3
 var rampUpIntervalMillisec = 150 // time to wait before running the next selects
-var rampUpSelects = 50           //number of selects to run every ramp up interval
+var rampUpSelects = 25           //number of selects to run every ramp up interval
 var dbUser = "root"
 var dbPassword = "root"
 var dbHost = "127.0.0.1"
 var dbPort = "3306"
 var selectString string
 var wg sync.WaitGroup
+var mysqlCommandFilterKeyword = "mysqlpooltesting"
+var mx sync.Mutex
+var statsCollectionMs int
+var stats statsT
+var statsHistory struct {
+	MysqlMaxOpenConns         int
+	MaxOpenConns              int
+	MaxIdleConns              int
+	ConnMaxLifetime           int
+	ConnMaxIdleTime           int
+	SelectsToRun              int
+	SelectSleep               int
+	RampUpIntervalMs          int
+	RampUpSelects             int
+	MysqlCommandFilterkeyword string
+	Stats                     []statsT
+}
 
 func main() {
+	//TODO PASS VALUES WE WANT TO CHANGE WITH COMMAND AND RETRIEVE THEM HERE BEFORE STARTING
+
 	connect()
 	defer closeConnection()
 
+	pushConfigValues()
+	initialiseStatsHistory()
+
+	wgAdd("farm")
+	selectFarm()
+	wg.Wait()
+}
+
+func initialiseStatsHistory() {
+	statsHistory.MysqlMaxOpenConns = mysqlMaxOpenConns
+	statsHistory.MaxOpenConns = maxOpenConns
+	statsHistory.MaxIdleConns = maxIdleConns
+	statsHistory.ConnMaxLifetime = connMaxLifetime
+	statsHistory.ConnMaxIdleTime = connMaxIdleTime
+	statsHistory.SelectsToRun = selectsToRun
+	statsHistory.SelectSleep = selectSleepSec
+	statsHistory.RampUpIntervalMs = rampUpIntervalMillisec
+	statsHistory.RampUpSelects = rampUpSelects
+	statsHistory.MysqlCommandFilterkeyword = mysqlCommandFilterKeyword
+}
+
+func pushConfigValues() {
 	setMaxOpenConns(maxOpenConns)
 	setMaxIdleConns(maxIdleConns)
 	setConnMaxLifetime(connMaxLifetime)
 	setConnMaxIdleTime(connMaxIdleTime)
 
 	setMysqlMaxOpenConns(mysqlMaxOpenConns)
-	selectString = fmt.Sprintf("select sleep(%d);", selectSleepSec)
-	wgAdd("farm")
-	fmt.Println("🙂")
-	fmt.Println("Farming...")
-	selectFarm()
-	fmt.Println("Waiting...")
-	fmt.Printf("%v", db.Stats())
-	wg.Wait()
-	go fmt.Print("HELLO!")
+	selectString = fmt.Sprintf("select sleep(%d); -- %s", selectSleepSec, mysqlCommandFilterKeyword)
 }
 
 func selectFarm() {
@@ -62,13 +105,9 @@ func selectFarm() {
 }
 
 func wgDone(who string) {
-	fmt.Printf("[%s 😈]", who)
-
 	wg.Done()
 }
 func wgAdd(who string) {
-	fmt.Printf("[%s 🙂]", who)
-
 	wg.Add(1)
 }
 
@@ -78,6 +117,10 @@ func selectRun(sequence int) {
 	if err != nil {
 		panic(err)
 	}
+	mx.Lock()
+	selectsCompleted++
+	mx.Unlock()
+
 }
 
 func closeConnection() {
@@ -101,6 +144,7 @@ func setMaxOpenConns(v int) {
 	maxOpenConns = v
 	db.SetMaxOpenConns(v)
 }
+
 func getMaxOpenConns() int {
 	return maxOpenConns
 }
@@ -131,4 +175,16 @@ func getConnMaxIdleTime() int {
 func setMysqlMaxOpenConns(v int) {
 	sql := fmt.Sprintf("set global max_connections = %d;", v)
 	db.Exec(sql)
+}
+
+func getCurrentOpenConns() int {
+	return db.Stats().OpenConnections
+}
+
+func getCurrentIdleConns() int {
+	return db.Stats().Idle
+}
+
+func getCurrentInUseConns() int {
+	return db.Stats().InUse
 }
